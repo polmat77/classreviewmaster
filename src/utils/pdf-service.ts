d17c4883a4b50bcf24881c65c5990b6bb56ed5ca
polmat77 <mathieupol@gmail.com>
2025-05-08 19:56:16 +0200

    Fix PDF.js worker loading

--- src/utils/pdf-service.ts
+++ src/utils/pdf-service.ts
@@ -1,8 +1,7 @@
-
 import * as pdfjs from 'pdfjs-dist';
 
 // Set the worker source for PDF.js
-const PDFJS_CDN_URL = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.worker.min.js";
+const PDFJS_CDN_URL = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120";
 
 /**
  * Initialize PDF.js worker
